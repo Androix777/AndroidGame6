2976b3184e81588febda669761be3ed85b21207f
Legio <zum.q@mail.ru>
2020-06-30 01:32:33 +0500

    Добавлен триггер при получении урона.

---
+++ Assets/Scripts/ChangeColorTrigger.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.ParticleSystem;
+
+public class ChangeColorTrigger : MonoBehaviour, ITrigger
+{
+    [SerializeField] private ParticleSystem particle;
+    [SerializeField] private MinMaxGradient gradient;
+    private ColorOverLifetimeModule colorOverLifetime;
+    [SerializeField] private MinMaxGradient gradientBack;
+    [SerializeField] private bool backColor;
+    [SerializeField] private bool modeOneGetColor;
+    [SerializeField] private float time;
+
+    private void Start()
+    {
+        colorOverLifetime = particle.colorOverLifetime;
+        if (modeOneGetColor && backColor)
+        {
+            gradientBack = colorOverLifetime.color;
+        }
+
+    }
+    private void Update()
+    {
+        gradient.mode = ParticleSystemGradientMode.Gradient;
+    }
+
+    public void ActivateEffect()
+    {
+        if (!modeOneGetColor && backColor)
+        {
+            gradientBack = colorOverLifetime.color;
+        }
+        colorOverLifetime.color = gradient;
+        if (backColor) StartCoroutine(BackColor());
+    }
+
+    IEnumerator BackColor()
+    {
+        yield return new WaitForSeconds(time);
+        colorOverLifetime.color = gradientBack;
+    }
+}
