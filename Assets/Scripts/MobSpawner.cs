bd90c8532e196c77d23d106f5991b750b07c6f8e
Androix777 <androix77@gmail.com>
2020-07-03 00:05:23 +0500

    Добавлен спавнер мобов

---
+++ Assets/Scripts/MobSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobSpawner : MonoBehaviour
+{
+    public Shooter shooter;
+    public GameObject[] mobs;
+    public float spawnRate;
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnRate, spawnRate);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        shooter.projectile = mobs[Random.Range(0,mobs.Length)];
+        shooter.Shoot(Random.Range(0.0f, 360.0f), true);
+    }
+}
