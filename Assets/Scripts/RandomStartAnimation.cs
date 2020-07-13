9d08961f3d2d2cef7f7a667d99f355ffbe79f54a
Legio <zum.q@mail.ru>
2020-07-13 15:37:23 +0500

    Добавлены частицы для боссов.

---
+++ Assets/Scripts/RandomStartAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStartAnimation : MonoBehaviour
+{
+    [SerializeField] float timeStart = 0;
+    [SerializeField] float timeEnd = 2;
+    [SerializeField] Animator animator;
+    void Start()
+    {
+        animator.Play("Tentacle_part", 0, Random.Range(0f,2f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
