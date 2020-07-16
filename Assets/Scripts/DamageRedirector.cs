af2db60132a5ceebd144077809d3bf000f389091
Androix777 <androix77@gmail.com>
2020-07-17 03:46:49 +0500

    Добавлен DamageRedirector

---
+++ Assets/Scripts/DamageRedirector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageRedirector : MonoBehaviour, IDamageable
+{
+    public IDamageable target;
+    [SerializeField] private Status _status = Status.Enemy;
+    public Status Status
+    {
+        get
+        { 
+            return _status; 
+        }
+        set
+        { 
+            _status = value; 
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        target.TakeDamage(damage);
+    }
+}
