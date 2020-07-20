a82c88415d97d586cf6254acc67382bc8c18827e
Legio <zum.q@mail.ru>
2020-07-20 05:06:34 +0500

    Добавлены звуки

---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour, ITrigger
+{
+    [SerializeField] AudioSource audioComponent;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ActivateEffect()
+    {
+        if (audioComponent != null)
+            audioComponent.Play();
+    }
+}
