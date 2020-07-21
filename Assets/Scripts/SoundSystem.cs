6dae9c3297037e44822b5c25599b585b100228bc
Legio <zum.q@mail.ru>
2020-07-21 06:58:44 +0500

    Добавлена система звука.

---
+++ Assets/Scripts/SoundSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public static class SoundSystem
+{
+    private static float volume = 1;
+    private static float volumeMusic = 1;
+    private static float volumeSounds = 1;
+
+    public static UnityEvent volumeChanged;
+    public static UnityEvent volumeMusicChanged;
+    public static UnityEvent volumeSoundsChanged;
+    public static float Volume
+    {
+        get { return volume; }
+        set
+        {
+            volume = value;
+            volumeChanged.Invoke();
+        }
+    }
+
+    public static float VolumeMusic
+    {
+        get { return volumeMusic; }
+        set
+        {
+            volumeMusic = value;
+            volumeMusicChanged.Invoke();
+        }
+    }
+
+    public static float VolumeSounds
+    {
+        get { return volumeSounds; }
+        set
+        {
+            volumeSounds = value;
+            volumeSoundsChanged.Invoke();
+        }
+    }
+}
--- Assets/Scripts/SoundTrigger.cs
+++ Assets/Scripts/SoundTrigger.cs
@@ -5,9 +5,27 @@ using UnityEngine;
 public class SoundTrigger : MonoBehaviour, ITrigger
 {
     [SerializeField] AudioSource audioComponent;
+
+    [SerializeField] float volumeBase = 1;
+    [SerializeField] AudioType type;
+
     void Start()
     {
+        if (audioComponent != null)
+            volumeBase = audioComponent.volume;
         
+        switch(type)
+        {
+            case AudioType.All:
+                SoundSystem.volumeChanged.AddListener(ChangeVolume);
+            break;
+            case AudioType.Sounds:
+                SoundSystem.volumeSoundsChanged.AddListener(ChangeVolume);
+            break;
+            case AudioType.Music:
+                SoundSystem.volumeMusicChanged.AddListener(ChangeVolume);
+            break;
+        }
     }
 
     // Update is called once per frame
@@ -16,6 +34,24 @@ public class SoundTrigger : MonoBehaviour, ITrigger
         
     }
 
+
+    void ChangeVolume()
+    {
+        float volume = 0;
+        switch(type)
+        {
+            case AudioType.All:
+                volume = SoundSystem.Volume;
+            break;
+            case AudioType.Sounds:
+                volume = SoundSystem.VolumeSounds;
+            break;
+            case AudioType.Music:
+                volume = SoundSystem.VolumeMusic;
+            break;
+        }
+        audioComponent.volume = volumeBase * volume;
+    }
     public void ActivateEffect()
     {
         if (audioComponent != null)
