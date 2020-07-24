edbfe82ce6dc99f4e3d3b65206c305cb7783a1e4
Legio <zum.q@mail.ru>
2020-07-24 15:02:40 +0500

    Уменьшение громкости музыки

---
+++ Assets/Scripts/Items/ItemTripleShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTripleShoot : MonoBehaviour
+{
+    
+    void Start()
+    {
+        gameObject.GetComponent<PlayerController>().shooter.numOfGuns += 2;
+        gameObject.GetComponent<PlayerController>().shooter.angleBetweenGuns = 10;
+        gameObject.GetComponent<PlayerController>().shooter.projectile.GetComponent<DelayedDeath>().time *=0.25f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
