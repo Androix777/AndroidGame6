42d75c1b61f143c77fc9d5cf1b1dd1693fa14e87
Legio <zum.q@mail.ru>
2020-08-21 05:55:53 +0500

    Исправлен предмет

--- Assets/Scripts/ItemsComponents/ShotGunItem1.cs
+++ Assets/Scripts/ItemsComponents/ShotGunItem1.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 public class ShotGunItem1 : MonoBehaviour, ITrigger
 {
-    
+
+    [SerializeField]float damagePlus = 10;
+    [SerializeField]float lifeTimeBullet = 0.85f;
+    [SerializeField]float fireRatePlus = -0.1f;
+    [SerializeField] float timeToBuff = 0.5f;
+
     void Start()
     {
+        gameObject.GetComponent<TriggerActivator>().triggers.Add(new Trigger(EventType.Shoot, this));
         foreach (Shooter shooter in gameObject.GetComponent<Core>().combatShooters)
         {
-            shooter.fireRateValue.AddIncrease(-0.1f);
-            shooter.projectile.GetComponent<DelayedDeath>().time *= 0.85f;
-            shooter.projectile.GetComponent<DamageDealer>().damageValue.AddIncrease(10);
+            shooter.fireRateValue.AddIncrease(fireRatePlus);
+            shooter.projectile.GetComponent<DelayedDeath>().time *= lifeTimeBullet;
         }
+        StartCoroutine(DamageUp());
     }
 
     public void ActivateEffect()
     {
-        gameObject.GetComponent<PlayerController>().shooter.projectile.GetComponent<DamageDealer>().damageValue.AddIncrease(-10);
+        gameObject.GetComponent<PlayerController>().shooter.projectile.GetComponent<DamageDealer>().damageValue.AddIncrease(-damagePlus);
+        StartCoroutine(DamageUp());
     }
 
+    IEnumerator DamageUp()
+    {
+        yield return new WaitForSeconds(timeToBuff);
+        foreach (Shooter shooter in gameObject.GetComponent<Core>().combatShooters)
+        {
+            shooter.projectile.GetComponent<DamageDealer>().damageValue.AddIncrease(damagePlus);
+        }
+    }
 
     void Update()
     {
