9de0ebcdcdb2e40d02888122e819832b25fb73de
Legio <zum.q@mail.ru>
2020-08-21 05:56:09 +0500

    Добавлен предмет

---
+++ Assets/Scripts/ItemsComponents/ShotGunItem2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGunItem2 : MonoBehaviour
+{
+    [SerializeField] float speed = -1f;
+    [SerializeField] float speedIncrease = -0.2f;
+    void Start()
+    {
+        foreach (DamageDealer damageDealer in gameObject.GetComponent<Core>().spawningDamage)
+        {
+            damageDealer.gameObject.AddComponent<ShotGunItem2Bullet>();
+
+            if (damageDealer.GetComponent<Move>())
+            {
+                damageDealer.GetComponent<Move>().speedValue.AddValue(speed);
+                damageDealer.GetComponent<Move>().speedValue.AddIncrease(speedIncrease);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ItemsComponents/ShotGunItem2Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGunItem2Bullet : MonoBehaviour, ITrigger
+{
+    [SerializeField] float damageMultiplicator = 1;
+    [SerializeField] float radius = 1;
+    float damage;
+    
+    public void Start()
+    {
+        if (gameObject.GetComponent<DamageDealer>())
+        {
+            damage = gameObject.GetComponent<DamageDealer>().damageValue.GetStatValue();
+        }
+
+        if (gameObject.GetComponent<TriggerActivator>())
+        {
+            gameObject.GetComponent<TriggerActivator>().triggers.Add(new Trigger(EventType.Death, this));
+        }
+    }
+
+    public void ActivateEffect()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, radius);
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.GetComponent<Life>())
+            {
+                hitCollider.GetComponent<Life>().TakeDamage(damage * damageMultiplicator);
+            }
+        }
+    }
+}
