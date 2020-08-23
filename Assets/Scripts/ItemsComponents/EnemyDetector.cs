0c41e28f2359ce00544643b7d5555842af91f61a
Legio <zum.q@mail.ru>
2020-08-24 02:51:13 +0500

    Изменён герой, добавлен предмет

---
+++ Assets/Scripts/ItemsComponents/EnemyDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDetector : MonoBehaviour
+{
+    public int numberEnemy = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        numberEnemy++;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        numberEnemy--;
+    }
+}
--- Assets/Scripts/ItemsComponents/ShotGunItem12.cs
+++ Assets/Scripts/ItemsComponents/ShotGunItem12.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class ShotGunItem12 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject enemyDetector;
+    [SerializeField] EnemyDetector detector;
+
+    [SerializeField] float numberBullet = 1f;
+    [SerializeField] float numberBulletIncrease = 0.2f;
+
+    bool activeEffect = false;
+
     void Start()
     {
-        
+        detector = GetComponentInChildren<EnemyDetector>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (detector != null)
+        {
+            if (detector.numberEnemy > 0 != activeEffect)
+            {
+                activeEffect = detector.numberEnemy > 0;
+                foreach (Shooter shooter in gameObject.GetComponent<Core>().combatShooters)
+                {
+                    shooter.fireRateValue.AddValue(numberBullet * (activeEffect ? 1:-1)) ;
+                    shooter.fireRateValue.AddIncrease(numberBulletIncrease * (activeEffect ? 1 : -1));
+                }
+            }
+        }
         
     }
 }
