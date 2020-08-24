a832faef9a531af62a29e65a11c894d81d01e5f3
Legio <zum.q@mail.ru>
2020-08-24 15:39:11 +0500

    Перемещены файлы

---
+++ Assets/Scripts/ItemsComponents/ShotGun/ShotGunItem5.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGunItem5 : MonoBehaviour
+{
+    [SerializeField] float speedFire = -1f;
+    [SerializeField] float speedFireIncrease = -0.2f;
+
+    [SerializeField] Component debuffComponent;
+
+    void Start()
+    {
+
+        foreach (Shooter shooter in gameObject.GetComponent<Core>().combatShooters)
+        {
+            shooter.fireRateValue.AddValue(speedFire);
+            shooter.fireRateValue.AddIncrease(speedFireIncrease);
+        }
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
